e30c790f6edea80d82eda36fe555022e943815b6
Carsen Ball <ballcarsen@gmail.com>
2018-09-19 15:40:02 -0600

    renamed DFS to Search, made a super Search class, DFS and BFS inheir

--- src/Search.py
+++ src/Search.py
@@ -1,14 +1,19 @@
-class DFS:
+class Search:
     def __init__(self, map, frontier):
         self.map = map
         self.frontier = frontier
 
-    # run search algorithm
+    # print maze to make sure map has been filled
+    def printMap(self):
+        for row in self.map:
+            for node in row:
+                print(node.char, end='')
+            print()
     def search(self):
         searchComplete = False
         while not searchComplete:
             # pop node off of frontier stack
-            frontierNode = self.frontier.pop()
+            frontierNode = self.remove()
             frontierNode.char = '.'  # . indicates space has been visited
             for neighbor in frontierNode.neighbors:
                 # if neighbor hasn't already been visited
@@ -24,12 +29,19 @@ class DFS:
             self.printMap()
             print("\n")
 
-    # print maze to make sure map has been filled
-    def printMap(self):
-        for row in self.map:
-            for node in row:
-                print(node.char, end='')
-            print()
+    def remove(self):
+        pass
+
+class DFS(Search):
+    #The order in which nodes are removed from the stack
+    def remove(self):
+        return self.frontier.pop()
+
+class BFS(Search):
+    def remove(self):
+        return self.frontier.pop(0)
+
+
 
 
 
